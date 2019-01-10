cada60f0fff5e47dde9aeb496cbb57041d4eb0d8
IsakHuddinge <isak.nywall@edu.huddinge.se>
2019-01-10 16:08:23 +0100

    stuff idk

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,15 +5,23 @@ var noteTitle = $('#noteTitle').val();
 $(function(){
     $.ajax({
         method: 'post',
-        url: 'https://api=notey.herokuapp.com/api/1.0/notes',
-        contentType: "application/json",
+        url: 'https://api-notey.herokuapp.com/api/1.0/notes',
+        // Switch to token
         data:{
-            userName:'tuva'
+            id:'5c371e57451d0200171adb3d'
         },
         success: function(notes){
-            console.log(notes);
+            $.each(notes, function(index, value){
+                var listItem = '<button type="button" value="'
+                listItem += value['_id']
+                listItem +='" class="list-group-item list-group-item-action">';
+                listItem += value['heading'];
+                listItem += '</button>';
+                $("#notey-list").append(listItem)
+            });
         },
         error: function(error){
+            console.log("failure");
             console.log(error);
         }
     });
@@ -24,12 +32,13 @@ $(function(){
     $("#firstSaveBtn").click(function(){
         console.log('notey was saved');
         // save title and content to database
+        // Currently does not work
         $.ajax({
             method: 'post',
             url: 'https://api-notey.herokuapp.com/api/1.0/notes/create',
             contentType: "application/json",
             data:{
-                userName:'tuva',                
+                id:'5c371e57451d0200171adb3d',                
                 heading: $("#notetitle").val,
                 content: $("#content").val,  
                 date:'e',                 
