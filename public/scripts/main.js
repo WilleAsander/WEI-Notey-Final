4882211d7cd9f22ab470bdb146ae23b00a860337
IsakHuddinge <isak.nywall@edu.huddinge.se>
2019-01-11 16:48:57 +0100

    move localstorage.getitem to main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,4 @@
-// Token for test account. Change this to the token given on login.
-var token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1YzM3MWU1NzQ1MWQwMjAwMTcxYWRiM2QiLCJ1c2VyTmFtZSI6InRlc3QiLCJlbWFpbCI6InRlc3QiLCJwYXNzd29yZCI6IiQyYSQxMCRzNWQxQkVPSllWWkxCQ0Z3Y0Zkei5PbFIxZXhwWHRUV3lxZTJZdFVMd014YzNhSWlVd0FuQyIsIl9fdiI6MH0.8cECfQfC87TjEM2bbI3OQGkQfv4HMHOixTIZWu-9pMg"
+var token = localStorage.getItem('userToken');
 
 // Fetches all notes on site load
 $(fetchNoteys);
