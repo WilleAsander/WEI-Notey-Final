378a5bce1ca27389894986ae9f039c7e5a385211
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2019-01-18 13:56:30 +0100

    replaced all popup windows with custom sliders

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,5 @@
 var error;
 var token = localStorage.getItem('userToken');
-// We should add a check to see if the logged in user is valid. but we can't since the api doesn't provide us with something like that.
 if (token === null){
     logout();
 }
@@ -67,10 +66,9 @@ function fetchNoteys(){
     
 }
 
-$(function(){
-    $("#saveNotey").click(function(){
+function createNote(){
         var title = $("#noteTitle").val();
-        var text = $("#content").val();
+        var text = CKEDITOR.instances.content.getData();
         var noteDate = new Date();
         var dd = noteDate.getDate();
         var mm = noteDate.getMonth()+1; //January is 0!
@@ -104,13 +102,66 @@ $(function(){
                 $("#noteTitle").val('');
                 $("#content").val('');
                 fetchNoteys();
+                transitionClose();
             },
             error: function(error){
                 var err = JSON.parse(error.responseText);
+                console.log(err);
             },
         });
-    });
-});
+}
+
+function transitionAdd(){
+    if($("#createNotey").is(":visible")){
+        return;
+    }
+    
+    else if($("#viewNotey").is(":visible") && $("#editNotey").is(":visible")){
+        closeAll();
+        $('#createNotey').addClass('magictime slideUpReturn');
+        setTimeout(function(){
+            $('#createNotey').removeClass('magictime slideUpReturn');
+        }, 1000);
+        $('#createNotey').css('display', 'block');
+    }
+    else if($("#editNotey").is(":visible")){
+        updateNoteyClose();
+        $('#createNotey').addClass('magictime slideUpReturn');
+        setTimeout(function(){
+            $('#createNotey').removeClass('magictime slideUpReturn');
+        }, 1000);
+        $('#createNotey').css('display', 'block');
+    }
+    else if($("#viewNotey").is(":visible")){
+        closeNotey();
+        $('#createNotey').addClass('magictime slideUpReturn');
+        setTimeout(function(){
+            $('#createNotey').removeClass('magictime slideUpReturn');
+        }, 1000);
+        $('#createNotey').css('display', 'block');
+    }
+    else{
+        $('#createNotey').addClass('magictime slideUpReturn');
+        setTimeout(function(){
+            $('#createNotey').removeClass('magictime slideUpReturn');
+        }, 1000);
+        $('#createNotey').css('display', 'block');
+    }
+    
+
+}
+
+function transitionClose(){
+    $('#createNotey').addClass('magictime slideUp');
+    setTimeout(function(){
+        $('#createNotey').removeClass('magictime slideUp');
+        $('#createNotey').css('display', 'none');
+    }, 1000);
+
+}
+
+
+
 $(function(){
     $("#logout").click(function(){
         logout();
@@ -125,21 +176,55 @@ function logout(){
 
 function openNotey(button){
     generateNote(button.value);
-    $("#updateTitle").hide();
-    $("#displayTitle").show();
-    
-    $("#updateContent").hide();
-    $("#displayContent").show();
-    
-    $("#updateNotey").show();
-    $("#saveUpdatedNotey").hide();
+    if($("#viewNotey").is(":visible")){
+        return;
+    }
+    else{
+        $('#viewNotey').addClass('magictime slideUpReturn');
+        setTimeout(function(){
+            $('#viewNotey').removeClass('magictime slideUpReturn');
+        }, 1000);
+        $('#viewNotey').css('display', 'block');
+    }
+}
 
-    $("#closeNotey").show();
-    $("#cancelUpdateNotey").hide();
+function closeNotey(){
+    $('#viewNotey').addClass('magictime slideUp');
+    setTimeout(function(){
+        $('#viewNotey').removeClass('magictime slideUp');
+        $('#viewNotey').css('display', 'none');
+    }, 1000);
+}
 
-    $("#deleteNotey").show();
+function updateNotey(){
+    if($("#editNotey").is(":visible")){
+        return;
+    }
+    else{
 
-    $("#myUpdateModal").modal('toggle');
+        $('#editNotey').addClass('magictime slideUpReturn');
+        setTimeout(function(){
+            $('#editNotey').removeClass('magictime slideUpReturn');
+        }, 1000);
+        $('#editNotey').css('display', 'block');
+        $('#saveUpdatedNotey').css('display', 'inline-block');
+        $('#cancelUpdateNotey').css('display', 'inline-block');
+        $('#viewFooter').css('display', 'none');
+    }
+}
+
+function updateNoteyClose(){
+    $('#editNotey').addClass('magictime slideUp');
+    setTimeout(function(){
+        $('#editNotey').removeClass('magictime slideUp');
+        $('#editNotey').css('display', 'none');
+    }, 1000);
+    $('#viewFooter').css('display', 'block');
+}
+
+function closeAll(){
+    updateNoteyClose();
+    closeNotey();
 }
 
 function generateNote(id){
@@ -152,10 +237,11 @@ function generateNote(id){
             var title = result.heading;
             var content = result.content;
             
-            $("#displayTitle").html(title);
-            $("#updateTitle").val(title);
-            $("#displayContent").html(content);
-            $("#updateContent").val(content);
+            $("#viewTitle").html(title);
+            $("#editTitle").val(title);
+            $("#viewBody").html(content);
+            $("#editContent").val(content);
+            CKEDITOR.instances.editContent.setData(content);
             $("#deleteNotey, #saveUpdatedNotey").val(id);
 
         },
@@ -171,6 +257,7 @@ function deleteNotey(id){
         url: 'https://api-notey.herokuapp.com/api/1.0/notes/delete/' + id,
         success: function(result){
             fetchNoteys();
+            closeNotey();
         },
         error: function(error){
         }
@@ -178,29 +265,10 @@ function deleteNotey(id){
     });
 };
 
-// Switch to edit mode
-$(function(){
-    $("#updateNotey").click(function(){
-        $("#displayTitle").hide();
-        $("#updateTitle").show();
-
-        $("#displayContent").hide();
-        $("#updateContent").show();
-
-        $("#updateNotey").hide();
-        $("#saveUpdatedNotey").show();
-
-        $("#closeNotey").hide();
-        $("#cancelUpdateNotey").show();
-
-         $("#deleteNotey").hide();
-    });
-});
-
 $(function(){
     $("#saveUpdatedNotey").click(function(){
-        var title = $("#updateTitle").val();
-        var text = $("#updateContent").val();
+        var title = $("#editTitle").val();
+        var text = CKEDITOR.instances.editContent.getData();
         var noteDate = new Date();
         var dd = noteDate.getDate();
         var mm = noteDate.getMonth()+1; //January is 0!
@@ -225,34 +293,21 @@ $(function(){
                 content: text,
                 date: noteDate
         };
-
+        var id = this.value;
         $.ajax({
             method: 'PATCH',
             url: 'https://api-notey.herokuapp.com/api/1.0/notes/update/' + this.value,
             contentType: "application/json",
             data: JSON.stringify(noteData),
             success: function(result){
-                $("#displayTitle").html(title);
-                $("#displayContent").html(text);
-
-                $("#updateTitle").hide();
-                $("#displayTitle").show();
-
-                $("#updateContent").hide();
-                $("#displayContent").show();
-
-                $("#updateNotey").show();
-                $("#saveUpdatedNotey").hide();
-
-                $("#closeNotey").show();
-                $("#cancelUpdateNotey").hide();
-
-                $("#deleteNotey").show();
+                generateNote(id);
+                updateNoteyClose();
 
 
             },
             error: function(error){
                 var err = JSON.parse(error.responseText);
+                console.log(err);
             }
         });
     });
