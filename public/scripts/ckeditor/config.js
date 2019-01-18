9239fc3ba0285c1013e7cd244b9ea1ae86257039
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2019-01-18 15:55:27 +0100

    Removed about and maximize button from cdkeditor

--- public/scripts/ckeditor/config.js
+++ public/scripts/ckeditor/config.js
@@ -15,15 +15,13 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'links' },
 		{ name: 'insert' },
 		{ name: 'forms' },
-		{ name: 'tools' },
 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'others' },
 		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		{ name: 'colors' }
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
