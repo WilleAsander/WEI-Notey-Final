9d55e429202ef903d546030c1abfe1e48dcc9e6b
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2019-02-01 16:24:55 +0100

    Added transitions for index

--- routes/home.js
+++ routes/home.js
@@ -7,5 +7,8 @@ router.get('/login', homeController.Login);
 router.get('/register', homeController.Register);
 router.get('/start', homeController.Start);
 router.get('/error', homeController.Error);
+router.get('/create', homeController.Create);
+router.get('/read', homeController.Read);
+router.get('/edit', homeController.Edit);
 
 module.exports = router;
